7e295c5781e201ac43f96d07b563cf386dcbe69d
Kaamil Jasani <kaamilj@yahoo.com>
2018-12-08 20:08:50 +0000

    Updated urls so main_app is at root instead of main_app/

--- WebProject/urls.py
+++ WebProject/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('main_app/', include('main_app.urls')),
+    path('', include('main_app.urls')),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
