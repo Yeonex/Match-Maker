03d5d4c6f142283f44c1fa80e39c1d55f4b4a66b
daniyaal <danyaalbeg@gmail.com>
2018-12-08 22:34:48 +0000

    Added installed apps to settings lol

--- WebProject/settings.py
+++ WebProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app.app.Main_appConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
