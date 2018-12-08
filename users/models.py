1c194da3272ab515dde60be398a0c9a6180c85f6
daniyaal <danyaalbeg@gmail.com>
2018-12-08 21:48:26 +0000

    Models created

--- users/models.py
+++ users/models.py
@@ -1,18 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Profile(models.Model):
-    user = models.OnetoOneField(User, unique = True, on_delete = models.CASCADE)
-    profile_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
-    email = models.EmailField()
-    gender = models.CharField(choices = GENDER_TYPE, max_length=1)
-
-class Hobbies(models.Model):
-    name = models.CharField(max_length=200)
-
 # Model Options
 GENDER_TYPE = (
     ('M', 'Male'),
     ('F', 'Female')
 )
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, unique = True, on_delete = models.CASCADE)
+    profile_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+    email = models.EmailField()
+    gender = models.CharField(choices = GENDER_TYPE, max_length=1)
+    date_of_birth = models.DateField()
+    hobbies = models.ManyToMany('Hobbies')
+    requests = models.ManyToMany('Profile')
+    connections = models.ManyToMany('Profile')
+
+class Hobbies(models.Model):
+    name = models.CharField(choices = HOBBIES_LIST, max_length=200)
+
+#Creates an instance of profile whenever a user object is created
+@receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
+
+@receiver(post_save, sender = User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
