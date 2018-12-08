8289d041b301f30f275d4a29d327c10e9629a6bf
Daniyaalbeg <danyaalbeg@gmail.com>
2018-12-08 20:15:00 +0000

    Model updates

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OnetoOneField(User, unique=True, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+
+class Hobbies(models.Model):
