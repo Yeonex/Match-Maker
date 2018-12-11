5e6a02835ebaa176c45759ce2fc4e90400cb745a
Daniyaalbeg <danyaalbeg@gmail.com>
2018-12-11 17:27:20 +0000

    Gets for all user data and specific user info work kind of, Need to implement Hobbies

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,6 @@ app_name = 'users'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:user_id>/', views.user_info, name='user_info'),
-    path('like/<int:user_id>', views.liked_user, name='liked_user'), #liked_user
+    path('like/<int:user_id>/', views.liked_user, name='liked_user'), #liked_user
     path('liked/', views.get_liked_users, name='get_liked_users'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,18 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from .models import Profile, Hobbies
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-
+@require_http_methods(["GET", "POST"])
 def index(request):
     #get and post in index for posting
-    return HttpResponse("Hello, world. You're at the users index.")
+    if request.method == "GET":
+        json = []
+        users = User.objects.all()
+        for user in users:
+            j = {
+                "first_name" : user.first_name,
+                "last_name" : user.last_name,
+                "email" : user.email,
+                "profile_pic" : user.profile.profile_pic.url,
+                "gender" : user.profile.gender,
+                "date_of_birth" : user.profile.date_of_birth,
+                "hobbies" : "TODO"
+            }
+            json.append(j)
+    return JsonResponse(json, safe=False)
 
+@require_http_methods(["GET"])
 def user_info(request, user_id):
     #get request for user
-    return HttpResponse("User returned here")
+    if request.method == "GET":
+        user = get_object_or_404(User, pk=user_id)
+        json = {
+            "first_name" : user.first_name,
+            "last_name" : user.last_name,
+            "email" : user.email,
+            "profile_pic" : user.profile.profile_pic.url,
+            "gender" : user.profile.gender,
+            "date_of_birth" : user.profile.date_of_birth,
+            "hobbies" : "TODO"
+        }
+    return JsonResponse(json, safe=False)
 
 def get_liked_users(request):
     #Gets all users liked by this account
