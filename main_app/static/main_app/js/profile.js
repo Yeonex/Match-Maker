f23400651c79ec590d481848b8b7a4616071e890
Ian <ianzack67@gmail.com>
2018-12-13 15:01:13 +0000

    Profile hook up complete

---
+++ main_app/static/main_app/js/profile.js
@@ -0,0 +1,22 @@
+function getProfileCard(picture, name, age, gender, hobbies){
+	$("#name").text(name);
+	$("#gender").text(gender);
+	$("#dob").text(age);
+	$("#hobbies").text(hobbies.join(", "));
+	$("#profile-picture").css("background-image","url("+picture+")");
+}
+
+
+
+
+$(document).ready(function(){
+	$.ajax({
+		url:"/users",
+		type:"GET",
+		success:function(data){
+			var d = data.current_user;
+			var age = data.current_user.date_of_birth;
+			$('#info').append(getProfileCard(d.profile_pic, d.first_name + " " + d.last_name , age, d.gender, d.hobbies));
+		}
+	});
+});
