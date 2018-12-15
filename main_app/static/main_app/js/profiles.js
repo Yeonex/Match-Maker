9d221e12c417cf66f48ff9845dc5b184a6571e0d
Ian <ianzack67@gmail.com>
2018-12-15 00:28:50 +0000

    Searching works

--- main_app/static/main_app/js/profiles.js
+++ main_app/static/main_app/js/profiles.js
@@ -66,7 +66,8 @@ function getProfileCard(id, imageUrl, name, age, gender, hobbies, liked){
                 .append('<i class="fa-fw far fa-heart"></i><i class="fa-fw fas fa-heart"></i>')
             )
         )
-    );
+		
+    ).attr("id", "profile" + id);
 }
 
 function getSimilarity(a, b){
@@ -96,6 +97,16 @@ $(document).ready(function(){
                 var age = Math.abs(ageDate.getUTCFullYear() - 1970);
                 $('#profiles').append(getProfileCard(d[i].id,d[i].profile_pic, d[i].first_name, age, d[i].gender, d[i].hobbies, d[i].liked));
             }
+			$("#search").on("keyup", function(){
+				var search_res = $(this).val().toLowerCase();
+				for(let i = 0; i < d.length; i++){
+					if(d[i].first_name.toLowerCase().includes(search_res)){
+						$('#profile'+d[i].id).show();
+					}else{
+						$('#profile'+d[i].id).hide();
+					}
+				}
+			});
         }
     });
 	
