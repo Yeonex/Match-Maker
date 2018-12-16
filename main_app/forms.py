a3040fd21078f5aa42ab86966e10179f33c42331
Kaamil Jasani <kaamilj@yahoo.com>
2018-12-16 18:01:38 +0000

    Added a name field that can now be used in forms

--- main_app/forms.py
+++ main_app/forms.py
@@ -3,6 +3,56 @@ from django.contrib.auth import password_validation
 from django import forms
 from users.models import Profile
 
+class NameInput(forms.MultiWidget):
+    def __init__(self, attrs=None, first_attrs=None, last_attrs=None):
+        widgets = (
+            forms.TextInput(
+                attrs=attrs if first_attrs is None else first_attrs,
+            ),
+            forms.TextInput(
+                attrs=attrs if last_attrs is None else last_attrs,
+            ),
+        )
+        super().__init__(widgets, attrs=attrs)
+
+    template_name = 'main_app/forms/widgets/name.html'
+    def decompress(self, name):
+        if name:
+            return [name['first_name'], name['last_name']]
+        return ['', '']
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+
+        if not isinstance(value, list):
+            value = self.decompress(value)
+        
+        final_attrs = context['widget']['attrs']
+        final_attrs.pop('type', None)
+        id_ = final_attrs.get('id')
+        subwidgets = []
+        for i, widget in enumerate(self.widgets):
+            widget_name = '%s_%s' % (name, i)
+            try:
+                widget_value = value[i]
+            except IndexError:
+                widget_value = None
+            if id_:
+                widget.attrs['id'] = '%s_%s' % (id_, i)
+            subwidgets.append(widget.get_context(widget_name, widget_value, widget.attrs)['widget'])
+        context['widget']['subwidgets'] = subwidgets
+        return context
+
+class NameField(forms.MultiValueField):
+    def __init__(self, **kwargs):
+        super().__init__([
+            forms.CharField(), forms.CharField()
+        ], **kwargs)
+
+    def compress(self, data_list):
+        print(data_list)
+        return {'first_name':data_list[0], 'last_name':data_list[1]}
+
 class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length = 254, widget = forms.TextInput(attrs = {'class':'form-control'}))
     password = forms.CharField(label = "Password", widget = forms.PasswordInput(attrs = {'class':'form-control'}))
